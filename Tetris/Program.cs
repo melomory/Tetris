065f892da097383f624280abe2c78d6a8e5304f3
melomory <niko.den.vl@gmail.com>
2022-01-31 19:19:27 +0400

    Создан Hello World

---
+++ Tetris/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Tetris
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.SetWindowSize(40, 20);
+            Console.SetBufferSize(40, 20);
+
+            Console.ReadLine();
+        }
+    }
+}
