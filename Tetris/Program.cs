06e3448377de60672f89fbe1f5065aca9685da46
melomory <niko.den.vl@gmail.com>
2022-02-01 06:59:20 +0400

    Вывод точки на экран

--- Tetris/Program.cs
+++ Tetris/Program.cs
@@ -9,7 +9,20 @@ namespace Tetris
             Console.SetWindowSize(40, 30);
             Console.SetBufferSize(40, 30);
 
+            int x1 = 2;
+            int y1 = 3;
+            char c1 = '*';
+
+            Draw(x1, y1, c1);
+
+
             Console.ReadLine();
+        }
+
+        static void Draw(int x, int y, char c)
+        {
+            Console.SetCursorPosition(x, y); ;
+            Console.Write(c);
         }
     }
 }
